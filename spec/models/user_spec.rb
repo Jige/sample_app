f9c0af4e9a786e6888cbe8aca560a692b3c20a71
Jige <jean-g.vernay@pulsarinnovation.com>
2012-05-18 13:09:57 +0200

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,11 @@ describe User do
 
   it { should be_valid }
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
@@ -117,4 +122,9 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+  
 end
